347bcc630db774f910fce3a4e7d937c8203130ef
Carlita Wiggan <info@cwiggan.com>
2019-11-26 17:07:30 -0500

    intial

---
+++ route/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+route.get('/login', (req, res) => res.send('Login'));
+route.get('/register', (req, res) => res.send('Login'));
+
+
+module.exports = router;
